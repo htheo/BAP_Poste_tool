0bd4a8474ba59a7ea3f64d961f2caf29377e4e22
Gauthier FLICHY <gauthier.flichy@monpotager.com>
2016-10-11 18:58:35 +0200

    wip recettage

--- blocs/recettage-projet.php
+++ blocs/recettage-projet.php
@@ -5,9 +5,9 @@ if (isset($pseudo) && isset($donnees_projet[0])) {
         <h2>Finalisation du projet</h2>
         <div class="export">
             <a href="../admin.php?panel=export-pdf"><div class="button-recettage"><i class="export-icon"></i>Exporter le recettage</div></a>
-            <a href="../<?php echo $donnees_projet[0]['cdc_link'] ?>"><div class="button-recettage"><i class="export-icon"></i>Exporter le cahier des charge</div></a>
+            <a href="../../<?php echo $donnees_projet[0]['cdc_link'] ?>"><div class="button-recettage"><i class="export-icon"></i>Exporter le cahier des charge</div></a>
             <?php if($donnees_projet[0]['gantt_link'] != ""){ ?>
-                <a href="../<?php echo $donnees_projet[0]['cdc_link'] ?>"><div class="button-recettage"><i class="export-icon"></i>Exporter le planning de Gantt</div></a>
+                <a href="../../<?php echo $donnees_projet[0]['gantt_link'] ?>"><div class="button-recettage"><i class="export-icon"></i>Exporter le planning de Gantt</div></a>
             <?php
             }?>
         </div>
