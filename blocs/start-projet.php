62e15e5077164fc59e8aa452a30f4b86279e91ea
Gauthier FLICHY <gauthier.flichy@monpotager.com>
2016-10-11 11:08:28 +0200

    Form style + enregistrement des infos

--- blocs/start-projet.php
+++ blocs/start-projet.php
@@ -3,18 +3,19 @@ if (isset($pseudo) && isset($donnees_projet[0])){
     ?>
 
     <div class="start-projet">
-        <h2>1er Etape - A propos du projet</h2>
+        <h2>A propos du projet</h2>
 
         <form action="../admin.php?action=update&panel=edit-start-projet-v" name="start-form" method="post" enctype="multipart/form-data">
+            <div class="input-group fileUpload">
+                <span>Enregister le cahier des charges</span>
+                <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+                <input type="file" id="uploadBtn" class="upload" name="cdc" accept=".doc,.docx, .pdf, .txt, .pages, .odt">
+            </div>
+            <input id="uploadFile" placeholder="Choisissez un fichier" disabled="disabled" />
 
             <div class="input-group">
-                <label for="cdc">Cahier des charge</label><br>
-                <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
-                <input type="file" name="cdc" accept=".doc,.docx, .pdf, .txt, .pages, .odt">
-            </div>
-            <div class="input-group">
                 <label for="pitch">Pitch du projet</label><br>
-                <textarea name="pitch" cols="30" rows="2"></textarea>
+                <textarea name="pitch" rows="3"></textarea>
             </div>
             <div class="input-group">
                 <label for="deadline">Date de livraison</label><br>
@@ -23,7 +24,11 @@ if (isset($pseudo) && isset($donnees_projet[0])){
             <button type="submit">Valider</button>
         </form>
     </div>
-
+    <script type="application/javascript">
+        document.getElementById("uploadBtn").onchange = function () {
+            document.getElementById("uploadFile").value = this.value;
+        };
+    </script>
     <?php
 }
 ?>
--- forms/edit-start-projet-v.php
+++ forms/edit-start-projet-v.php
@@ -9,11 +9,7 @@ if(isset($pseudo) && $level<=3 || $_POST['cdc']){
             case 'update':
                 if(isset($_FILES['cdc']) AND isset($_POST['pitch']) AND isset($_POST['deadline'])){
                     $erreur = "";
-                  
                     $extensions_valides = array( 'doc' , 'docx' , 'pdf' , 'odt', 'pages', 'txt' );
-                    //1. strrchr renvoie l'extension avec le point (« . »).
-                    //2. substr(chaine,1) ignore le premier caractère de chaine.
-                    //3. strtolower met l'extension en minuscules.
                     $extension_upload = strtolower(  substr(  strrchr($_FILES['cdc']['name'], '.')  ,1)  );
                     if ( in_array($extension_upload,$extensions_valides) ){
                         $info = "Extension correcte";
@@ -31,18 +27,20 @@ if(isset($pseudo) && $level<=3 || $_POST['cdc']){
                         $nom = "cdc/{$nom_projet}.{$extension_upload}";
                         $resultat = move_uploaded_file($_FILES['cdc']['tmp_name'], $nom);
                     }
-                    $cdc_link = htmlentities($_FILES['cdc']['name']);
                     $pitch = htmlentities($_POST['pitch']);
                     $deadline=htmlentities($_POST['deadline']);
                     $id = $donnees_projet[0]['id'];
                     echo $id;
 
-                    $insert = db_update('projet_bap', array('cdc_link'=>$cdc_link, 'pitch'=>$pitch, 'deadline' => $deadline), array('id' => $id));
-                    
+                    $update = db_update('projet_bap', array('cdc_link'=>$nom, 'pitch'=>$pitch, 'deadline' => $deadline, 'statut' => 'en_cours'), array('id' => $id));
+
+                    if(isset($update)) {
+                        echo 'Validé ! <script>  window.location.href = "admin.php";</script> <a href="admin.php">Cliquez-ici pour valider</a>';
+                    }
                 }else{
-                    $name = NULL;
-                    $brief = NULL;
-                    $date = NULL;
+                    $nom = NULL;
+                    $pitch = NULL;
+                    $deadline = NULL;
                 }
 
                 break;
